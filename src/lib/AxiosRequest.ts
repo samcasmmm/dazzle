6cc0dec30cef3ccf80230e38cb3cd8b9b75a200b
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-12-07 02:05:43 +0530

    fixed errors

--- src/lib/AxiosRequest.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const DeleteUploadThingByUrl = async (url: string) => {
-  await axios.delete('api/uploadthing', {
-    data: {
-      url: url,
-    },
-  });
-};
